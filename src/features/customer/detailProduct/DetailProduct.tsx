1fa626c6248e2bb048d576c9bca0a514026ec6c3
thinh van bao <thinhvanbao312002@gmail.com>
2026-06-12 00:53:55 +0700

    fix

--- src/features/customer/detailProduct/DetailProduct.tsx
+++ src/features/customer/detailProduct/DetailProduct.tsx
@@ -223,7 +223,7 @@ function DetailProductPage() {
               {/* DESCRIPTION */}
               {product.introduce && (
                 <div className='mb-6 pb-6 border-b border-gray-200'>
-                  <p className='text-gray-700 leading-relaxed text-sm'>{product.introduce}</p>
+                  <p className='whitespace-pre-wrap break-words text-sm leading-relaxed text-gray-700'>{product.introduce}</p>
                 </div>
               )}
 
